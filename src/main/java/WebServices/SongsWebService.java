56c7c8bdc94921e4591c2828d53d3c8a1bd3bfcf
sebita22 <s_ferra3@hotmail.com>
2019-06-24 00:15:44 -0300

    primer intento

--- src/main/java/WebServices/SongsWebService.java
+++ src/main/java/WebServices/SongsWebService.java
@@ -13,10 +13,13 @@ import Models.Responses.GetSongsFailedResponseBody;
 import Models.Responses.GetSongsResponseBody;
 import Models.Song;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -29,29 +32,32 @@ public class SongsWebService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     //cambio tipo de genre
-    public Response getSongs(@QueryParam("author")String author, @QueryParam("genre") Genre genre) {
+    public void getSongs(@Suspended final AsyncResponse asyncResponse, @QueryParam("author")String author, @QueryParam("genre") Genre genre) {
         SongsController songsController = new SongsController();
         try {
             if(author != null){
                 if(genre != null){
                     List<Song> songs = songsController.getSongsFilteredByAuthorAndGenre(author, genre);            
-                    return Response.ok(new GetSongsResponseBody(songs)).build();
+                    asyncResponse.resume(Response.ok(new GetSongsResponseBody(songs)).build());
                 } else 
                 {
                     List<Song> songs = songsController.getSongsFilteredByAuthor(author);            
-                    return Response.ok(new GetSongsResponseBody(songs)).build();
+                    asyncResponse.resume(Response.ok(new GetSongsResponseBody(songs)).build());
                 }                
             } else 
             {
                 if(genre != null){
                     List<Song> songs = songsController.getSongsFilteredByGenre(genre);
-                    return Response.ok(new GetSongsResponseBody(songs)).build();
+                    asyncResponse.resume(Response.ok(new GetSongsResponseBody(songs)).build());
                 }
             }
-            List<Song> songs = songsController.getSongs();            
-            return Response.ok(new GetSongsResponseBody(songs)).build();
+            CompletableFuture.supplyAsync(() -> {
+                return songsController.getSongs();  
+            }).thenAccept(songs -> {
+                 asyncResponse.resume(Response.ok(new GetSongsResponseBody(songs)).build());
+            });
         } catch (Exception ex) {
-            return Response.status(500).entity(new GetSongsFailedResponseBody(ex.getMessage())).build();
+            asyncResponse.resume(Response.status(500).entity(new GetSongsFailedResponseBody(ex.getMessage())).build());
         }
     }
 }
